f28ad05aec9fbdb303acaf031f340c2cb9d0bb55
Chandan Taneja <ctaneja@EX2INDIA.LOCAL>
2022-04-20 10:31:42 +0530

    completed directives task

--- src/app/core/directives/input-numbers.directive.ts
+++ src/app/core/directives/input-numbers.directive.ts
@@ -1,11 +1,11 @@
-import { Directive, HostListener, Inject, Input } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[inputNumbers]'
 })
 export class InputNumbersDirective {
   @Input() allowNumbers: boolean = true;
-  @HostListener('keypress', ['$event'])
+  @HostListener('keypress')
   onKeyPress(event: KeyboardEvent) {
     const charCode = event.key.charCodeAt(0);
     if (charCode >= 48 && charCode <= 57) return true;
